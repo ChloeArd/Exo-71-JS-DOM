7c7822372afd4d451ed773b25002ede6a9054e28
ChloeArd <chloe.ardoise@gmail.com>
2020-12-04 09:43:17 +0100

    Exercice 71

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,16 @@
-let monDiv = .createElement('div');
+let monDiv = document.createElement('div');
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript";
+let premierDiv = document.getElementById('viewport').appendChild(monDiv);
 
-document.getElementById('viewport').appendChild(monDiv);
+let monDiv2 = document.createElement('div');
+monDiv2.style.backgroundColor = "red";
+monDiv2.style.width = 100 + "px";
+monDiv2.style.height = 100 + "px";
+premierDiv.appendChild(monDiv2);
+
+let monSpan = document.getElementById('aSupprimer');
+monSpan.remove(premierDiv);
+
 
 
 
